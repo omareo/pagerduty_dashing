51933023997013be7d563414eb97cadd99f9a1b9
Omar Ontiveros <omareo@gmail.com>
2016-10-11 16:22:47 -0700

    zendesk stuff- very messy code. will refactor next chance.'

--- jobs/pagerduty_incidents.rb
+++ jobs/pagerduty_incidents.rb
@@ -1,6 +1,10 @@
 require 'faraday'
 require 'json'
 
+# for Zendesk
+ require 'uri'
+ require 'net/http'
+
 url = ENV['PAGERDUTY_URL']
 api_key = ENV['PAGERDUTY_APIKEY']
 env_services = ENV['PAGERDUTY_SERVICES']
@@ -14,6 +18,11 @@ end
 
 triggered = 0
 acknowledged = 0
+
+zendesk_url = ENV['ZENDESK_URL']
+zendesk_view = ENV['ZENDESK_VIEW_UNASSIGNED']
+zednesk_view2 = ENV['ZENDESK_VIEW_PENDING']
+zendesk_auth = ENV['ZENDESK_AUTH']
 
 SCHEDULER.every '30s' do
   services.each do |key, value|
@@ -30,4 +39,30 @@ SCHEDULER.every '30s' do
     triggered = json['service']['incident_counts']['triggered']
     send_event("#{key}-triggered", value: triggered)
   end
+
+
+  con = Faraday.new	
+  res = con.get do | req|
+	req.url "#{zendesk_url}/api/v2/views/#{zendesk_view}/count.json" 
+	req.headers['Authorization'] = "#{zendesk_auth}"
+  end
+
+
+  zendesk_json = JSON.parse(res.body)
+  zendesk_triggered = 0
+  zendesk_triggered = zendesk_json['view_count']['value']
+  send_event("zendesk-triggered", value: zendesk_triggered)
+  
+  con = Faraday.new	
+  res2 = con.get do | req2|
+	req2.url "#{zendesk_url}/api/v2/views/#{zendesk_view2}/count.json" 
+	req2.headers['Authorization'] = "Basic #{zendesk_auth}"
+  end
+
+  
+  zendesk_json2 = JSON.parse(res2.body)
+  zendesk_pending_triggered = 0
+  zendesk_pending_triggered = zendesk_json2['view_count']['value']
+  send_event("zendesk_pending-triggered", value: zendesk_pending_triggered)
+
 end
